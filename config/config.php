9b781a5124ba486e791d2dc6dba08854efe0215b
Dealcraft <david.loh18.dl@gmail.com>
2022-02-25 18:01:58 +0100

    Config

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+    $CONFIG = [
+        'dbhost' => '',
+        'dbname' => '',
+        'dbuser' => '',
+        'dbpassword' => '',
+        'dbtableprefix' => '',
+        'passwordsalt' => '',
+        'trusteddomains' => [
+
+        ],
+        'version' => '',
+        'datadirectory' => '',
+        'installed' => false,
+        'language' => 'en',
+        'remember_login_lifetime' => 60*60*24*15,
+        'forgot_password_link' => 'https://example.org/account/password-reset',
+        'logo_url' => 'https://example.org/logo.png',
+        'blacklisted_files' => [
+            '.htaccess'
+        ],
+        'theme' => '',
+        'debug' => false,
+        'maintenance' => false
+    ];
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once 'config/config.php'; ?>
+<!doctype html>
+<html lang="<?= $CONFIG['language'] ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
